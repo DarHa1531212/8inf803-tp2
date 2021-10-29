1365f881e6c27ebd071fca83fbbb01643d7eb620
Vincent Gagnon <vincentgagnon53@gmail.com>
2021-10-29 10:14:26 -0400

    récupération des monstres

--- crawler.py
+++ crawler.py
@@ -10,19 +10,16 @@ main_page = requests.get('https://www.d20pfsrd.com/bestiary/bestiary-alphabetica
 soup = BeautifulSoup(main_page.content, 'lxml')
 
 hrefList = soup.find('ul', {'class': 'ogn-childpages'})
-#print(hrefList)
+# print(hrefList)
 
 for link in hrefList.find_all('a'):
     page = requests.get(link.get('href'))
     soup_bis = BeautifulSoup(page.content, 'lxml')
 
-    for link2 in hrefList.find_all():
-
-
-
-
-
-
-
-
-
+    for td in soup_bis.find_all('td'):
+        for h3 in td.find_all('h3'):
+            h3.extract()
+        for sup in td.find_all('sup'):
+            sup.extract()
+        for a in td.find_all('a', href=lambda href: href and "https://www.d20pfsrd.com/bestiary/" in href):
+            print(a)
