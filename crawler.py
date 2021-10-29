49ed45c4ed77fa42640e16c9de8797d1963fb844
PIER0318 <lefebvre.pierre0318@gmail.com>
2021-10-29 09:35:02 -0400

    début crawler

--- crawler.py
+++ crawler.py
@@ -1,2 +1,28 @@
-import bs4
+from bs4 import BeautifulSoup
+import requests
+import json
+
+monster = {
+    "sorts": []
+}
+
+main_page = requests.get('https://www.d20pfsrd.com/bestiary/bestiary-alphabetical/')
+soup = BeautifulSoup(main_page.content, 'lxml')
+
+hrefList = soup.find('ul', {'class': 'ogn-childpages'})
+#print(hrefList)
+
+for link in hrefList.find_all('a'):
+    page = requests.get(link.get('href'))
+    soup_bis = BeautifulSoup(page.content, 'lxml')
+
+    for link2 in hrefList.find_all():
+
+
+
+
+
+
+
+
 
